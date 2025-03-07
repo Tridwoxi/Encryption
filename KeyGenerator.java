d37cff75a3d4be1d4b60134d4150641738d4bf6d
Tridwoxi <dw.oxidane@gmail.com>
2025-03-07 13:50:09 -0500

    minimum working product

--- KeyGenerator.java
+++ KeyGenerator.java
@@ -1,20 +1,41 @@
+/* 
+    TODO:
+    - put everything in a queue
+    - documentation
+ */
+
 /**
     State machine that yields keys by churning original.
  */
 public class KeyGenerator {
     private String key;
+    private String left_half;
+    private String right_half;
+
+    public static void main(String[] args) {
+        String example = "aaaaaaaabbbbbbbbccccccccddddddddeeeeeeeeffffffffgggggggg";
+        KeyGenerator kg = new KeyGenerator(example);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(kg.next());
+        }
+    }
 
     public KeyGenerator(String key) {
         if (key.length() != 56) {
             throw new Error("key must be 56 bit binary string");
         }
-        this.key = key;
+        this.left_half = key.substring(0, 28);
+        this.right_half = key.substring(28, 56);
     }
 
     public String next() {
-        // REASSIGN KEY
+        left_half = rotate_left(left_half);
+        right_half = rotate_left(right_half);
+        key = left_half + right_half;
+        return key.substring(0, 32);
+    }
 
-        // YIELD
-        return key;
+    private String rotate_left(String string) {
+        return string.substring(1) + string.substring(0, 1);
     }
 }
