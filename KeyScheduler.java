ddf0d8391f0db0d64c85c7067d1c3bf062055a07
Tridwoxi <dw.oxidane@gmail.com>
2025-03-15 15:58:53 -0400

    finished KeyScheduler.java. documentation style is now arguably better; sometimes it's really weird explaining how the "rotate left" function "roates left". As the python programmers say, write documentation not comments, unless those comments are implementation notes or attributions or otherwise useful, but java documentation often feels like "what the print statement does is that it prints something"

--- KeyScheduler.java
+++ KeyScheduler.java
@@ -1,24 +1,48 @@
-/* 
-    TODO:
-    - put everything in a queue
-    - documentation
- */
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
-    State machine that yields keys by churning original.
+    The transformation function. Contains queue and stack of keys for encryption/decryption. 
+
+    @apiNote Instantiate with full key. Call {@code next_encryption_key} and {@code next_decryption_key} for round keys. Will return null if insufficeint keys for decryption. Either get the encryption keys all at once and then the decryption keys later (recommended) or call them alternating and mark them as pairs. Do not arbritrarily mix the order of getting encryption and decryption keys.
+
+    @implNote Created as a wrapper for {@link KeyGenerator}. Implementation using queue considered harmful but required by specification. 
  */
 public class KeyScheduler {
+    private KeyGenerator key_generator;
+    private Queue<String> forward;
+    private Stack<String> reverse;
+
+    public KeyScheduler(String full_key) {
+        key_generator = new KeyGenerator(full_key);
+        forward = new LinkedList<>();
+        reverse = new Stack<>();
+    }
+
+    public String next_encryption_key() {
+        String round_key = key_generator.next();
+        forward.add(round_key);
+        reverse.push(round_key);
+        return forward.remove();
+    }
+
+    public String next_decryption_key() {
+        return reverse.empty() ? null : reverse.pop();
+    }
+}
+
+/**
+    State machine that yields new keys.
+
+    @apiNote Instantiate with full key. Call {@code next} to get the next key. Should not be used directly but rather as part of {@link KeyScheduler}.
+
+    @implNote Implemented as a generator function. Done by churning the original with rotate_left and returning the first 32 bits as a round key. 
+ */
+class KeyGenerator {
     private String key;
     private String left_half;
     private String right_half;
-
-    public static void main(String[] args) {
-        String example = "aaaaaaaabbbbbbbbccccccccddddddddeeeeeeeeffffffffgggggggg";
-        KeyGenerator kg = new KeyGenerator(example);
-        for (int i = 0; i < 10; i++) {
-            System.out.println(kg.next());
-        }
-    }
 
     public KeyGenerator(String key) {
         if (key.length() != 56) {
